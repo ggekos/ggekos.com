f1fe94861c9c1aea3c0d4dfacd2f5901604feb38
Alexandre Colella <nozutbm@gmail.com>
2018-07-16 14:37:58 +0200

    adding blog wip

--- src/Controller/GgekosController.php
+++ src/Controller/GgekosController.php
@@ -6,6 +6,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 class GgekosController extends Controller
 {
@@ -14,10 +16,38 @@ class GgekosController extends Controller
      */
     public function index(KernelInterface $kernel)
     {
-        $values = Yaml::parseFile($kernel->getProjectDir().'/public/ggekos.yml');
+        return $this->render('ggekos/index.html.twig', 
+            Yaml::parseFile($kernel->getProjectDir().'/public/ggekos.yml'));
+    }
 
-        return $this->render('ggekos/index.html.twig', [
-            'values' => $values,
+    /**
+     * @Route("/blog", name="blogList")
+     */
+    public function list(KernelInterface $kernel)
+    {
+        $finder = new Finder();
+        $finder->files()->in($kernel->getProjectDir().'/public/blog/');
+
+        foreach ($finder as $file) {
+            $list[] = explode('.', $file->getFileName())[0];
+        }
+        
+        return $this->render('ggekos/list.html.twig', [
+            'list' => $list
         ]);
+    }
+
+    /**
+     * @Route("/blog/{slug}", name="blogSingle")
+     */
+    public function single(KernelInterface $kernel, Filesystem $filesystem, $slug)
+    {
+        $path = $kernel->getProjectDir().'/public/blog/'.$slug.'.yml';
+
+        if (!$filesystem->exists($path)) {
+            //404
+        }
+
+        return $this->render('ggekos/single.html.twig', Yaml::parseFile($path));
     }
 }
